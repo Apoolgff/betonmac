6d6a3d2891d6f9b1ffcae359363012fc662b49f2
Andres Romero <aromerosambucetti@gmail.com>
2024-07-25 01:00:44 -0300

    estilado del boton

--- src/components/Inicio/Hero.jsx
+++ src/components/Inicio/Hero.jsx
@@ -8,9 +8,7 @@ const Hero = () => {
                 <div className="hero-content">
                     <h1 className="hero-title">hormigones, <br />automatismos y<br />equipos productivos</h1>
                     <p className="hero-subtitle">Una Empresa De Ingenieria Con Experiencia Y Calidad</p>
-                    <button>
-                    <span>Contacto</span>
-                    </button>
+                    <div className='button-hexagon'><div className='button-content'></div></div>
 
                 </div>
                <img className='hero-video' src="/videos/concrete-gif-video.gif" alt="background de prueba" />
