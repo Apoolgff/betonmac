347a5413d8b72402717051c40949e0fff0ca2054
Andres Romero <aromerosambucetti@gmail.com>
2024-07-27 15:57:00 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 const socialLinks = [
   { url: 'https://instagram.com', icon: '/images/icons/instagram.png' },
+  { url: 'https://facebook.com/', icon: '/images/icons/facebook.png' },
   { url: 'https://linkedin.com', icon: '/images/icons/linkedin.png' },
   { url: 'https://web.whatsapp.com/', icon: '/images/icons/whatsapp.png' },
+  { url: 'https://facebook.com/', icon: '/images/icons/google-maps.png' },
 ];
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-layer">
-      <div className="footer-left">
-        <img src='https://betonmac.com/images/Imagenes/Logo/Logo.png' alt="Company Logo" className="footer-logo" />
-        <a href='https://betonmac.com/index.php/legal' className="footer-legal">Aviso Legal</a>
-      </div>
-      <div className="footer-center">
-        <div className="footer-socials">
-          {socialLinks.map((link, index) => (
-            <img key={index} href={link.url} className="footer-social-link" src={link.icon}  />
-          ))}
-        </div>
-      </div>
       <div className="footer-right">
+          <div className="footer-socials">
+            {socialLinks.map((link, index) => (
+              <img key={index} href={link.url} className="footer-social-link" src={link.icon}  />
+            ))}
+          </div>
+        </div>
+       
+      
+      <div className='farallelogram-container scale'>
+         
+          <div className='farallelogram farallelogram-c'></div>
+          <div className='farallelogram farallelogram-a'></div>
+          <div className='farallelogram farallelogram-b'></div>
+          <div className='farallelogram farallelogram-d'></div>
+          <div className='farallelogram farallelogram-e'></div>
+          <div className='farallelogram farallelogram-f'></div>  
+  </div>
+      
+        <div className="footer-center">
+        <nav className='nav-links-footer '>
+            <Link to="/" className="nav-item-footer">inicio</Link>
+            <Link to="/productos" className="nav-item-footer">Productos</Link>
+            <Link to="/institucional" className="nav-item-footer">Institucional</Link>
+            <Link to="/obras" className="nav-item-footer">Obras</Link>
+            <Link to="/contacto" className="nav-item-footer">Contacto</Link>
+            <Link to="/calendario" className="nav-item-footer">Calendario</Link>
+        </nav>
+   
+        </div>
         
-        <p className="footer-address">Av. Circunvalación Y Camino San Carlos Km 4,5.
-          <br />5000 Córdoba - Argentina -</p>
-        <p className="footer-phone">(+54 - 351) 569-3100 / (+54 - 351) 464-2107</p>
+    
+      <div>
+        
+       
       </div>
+
       </div>
+      <a href='https://betonmac.com/index.php/legal' className="footer-legal">Aviso Legal</a>
     </footer>
   );
 };
--- src/components/Inicio/Hero.jsx
+++ src/components/Inicio/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
                 <div className="hero-content">
                     <h1 className="hero-title">hormigones, <br />automatismos y<br />equipos productivos</h1>
                     <p className="hero-subtitle">Una Empresa De Ingenieria Con Experiencia Y Calidad</p>
-                    <div className='button-hexagon'><div className='button-content'></div></div>
+                    <div className='button-hexagon-white'></div>
 
                 </div>
                <img className='hero-video' src="https://betonmac.com/images/Imagenes/Portadas-plantas/Portada-Interior-Mina.jpg" alt="background de prueba" />
