2c7ab932b7f79a4c95fb7d1397eb22895515bccb
Andres Romero <aromerosambucetti@gmail.com>
2024-08-01 14:44:04 -0300

    arreglos de contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -15,15 +15,17 @@ const Contacto = () => {
   }, []);
 
   const ubication = [
-
     {title: 'argentina', image:'/public/images/paises/argentina.jpg'},
+    {title: 'uruguay', image:'/public/images/paises/uruguay.jpg'},
     {title: 'brasil', image:'/public/images/paises/brasil.jpg'},
     {title: 'ecuador', image:'/public/images/paises/ecuador.jpg'},
-    {title: 'peru', image:'/public/images/paises/peru.jpg'},
-    {title: 'uruguay', image:'/public/images/paises/uruguay.jpg'},
-    {title: 'paraguay', image:'/public/images/paises/paraguay.jpg'},
     {title: 'chile', image:'/public/images/paises/chile.jpg'},
     {title: 'bolivia', image:'/public/images/paises/bolivia.jpg'},
+    {title: 'peru', image:'/public/images/paises/peru.jpg'},
+    
+    
+    
+    
   ];
 
   const groupedContact = contact.reduce((acc, cont) => {
@@ -42,12 +44,25 @@ const Contacto = () => {
       <h2 className="contact-main-title">Contactos</h2>
 
       {/*Grilla de Contactos*/}
-      <div className="contact-grid">
+      <div className="contact-list">
         {ubication.map((ubi, index) => (
-          <div key={index} className="contact-card contact-card-`${ubi.title}`" style={{ backgroundImage: `url(${ubi.image})` }}>
-            <div className='contact-content'>
-              <h3>{ubi.title}</h3>
-              <div className="contact-details">
+          <details key={index} className="contact-details" >
+            <summary className='contact-summary'>
+              <h3 className='summary-title'>{ubi.title}</h3>
+              <div className='summary-button'>
+              <div className='button-hexagon-white'>
+                        <div className='harallelogram-container harallelogram-button'>
+                            <div className='harallelogram harallelogram-c animation-button-left'></div>
+                            <div className='harallelogram harallelogram-a animation-button-left'></div>
+                            <div className='harallelogram harallelogram-b animation-button-top'></div>
+                            <div className='harallelogram harallelogram-d animation-button-top'></div>
+                            <div className='harallelogram harallelogram-e animation-button-right'></div>
+                            <div className='harallelogram harallelogram-f animation-button-right'></div>
+                            <img className="button-hexagon-full"src="/public/images/icons/button-white-hover.png" alt="" />  
+                        </div>
+                    </div>
+                    </div>
+              <div className="contact-individual">
               {groupedContact[ubi] ? (
                 groupedContact[ubi].map(contact => (
                   <button 
@@ -62,8 +77,8 @@ const Contacto = () => {
                 <p>No hay contactos en este país.</p>
               )}
             </div>
-            </div>
-          </div>
+            </summary>
+          </details>
         ))}
       </div>
 
