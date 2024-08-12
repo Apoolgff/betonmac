37b53373e2ed1f8c8068dae4e87997d1ad28ae41
Andres Romero <aromerosambucetti@gmail.com>
2024-08-11 23:24:18 -0300

    estilando el contacto

--- src/components/contacto/ContactServices.jsx
+++ src/components/contacto/ContactServices.jsx
@@ -5,19 +5,19 @@ import './ContactServices.css';
 const ContactServices = () => {
 
     const via = [
-        {title: 'Ubicacion', 
+        {title: 'ubicacion', 
           text:'Av. Circunvalación y Camino San Carlos Km 4,5.5000 Córdoba - Argentina -' , 
           image:'/public/images/icons/location-large.png'
         },
-        {title: 'Telefono', 
+        {title: 'telefono', 
           text:'+54 351 569-3100' , 
           image:'/public/images/icons/phone-large.png'
         },
-        {title: 'Whatsapp', 
+        {title: 'whatsapp', 
           text:' +54 9 351 520-4466' , 
           image:'/public/images/icons/whatsapp-large.png'
         },
-        {title: 'Mail', 
+        {title: 'mail', 
           text:'betonmac@betonmac.com.ar' , 
           image:'/public/images/icons/mail-large.png'
         },
@@ -29,7 +29,7 @@ return (
         {via.map((medio, index) => (
           <div key={index} className="contact-service-card" >
            <img src={medio.image} alt={`icono de ${medio.title}`} />
-            <h4 >{medio.title}</h4>
+            <h4>{medio.title}</h4>
             <a href="">{medio.text}</a>
           </div>
         ))}
